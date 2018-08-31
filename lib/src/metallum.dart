e04c6d6fdd7b32a000d3e2eccd444fac6f8face2
Rodrigo Muniz <rodrigo.temiski1995@gmail.com>
2018-08-31 11:13:33 -0300

    Minor fixes to use Dart 2.0

--- lib/src/metallum.dart
+++ lib/src/metallum.dart
@@ -8,37 +8,37 @@ import 'package:metallum/src/wrapper/label.dart';
 getLatestAddedBands(int year, int month) async {
   String url = latestAddedBands(year, month);
   http.Response res = await http.get(url);
-  return parseLatestAddedBandsList(JSON.decode(res.body));
+  return parseLatestAddedBandsList(jsonDecode(res.body));
 }
 
 getLatestAddedLabels(int year, int month) async {
   String url = latestAddedLabels(year, month);
   http.Response res = await http.get(url);
-  return parseLatestAddedLabelsList(JSON.decode(res.body));
+  return parseLatestAddedLabelsList(jsonDecode(res.body));
 }
 
 getLatestAddedArtists(int year, int month) async {
   String url = latestAddedArtists(year, month);
   http.Response res = await http.get(url);
-  return parseLatestAddedArtistsList(JSON.decode(res.body));
+  return parseLatestAddedArtistsList(jsonDecode(res.body));
 }
 
 getLatestUpdatedBands(int year, int month) async {
   String url = latestUpdatedBands(year, month);
   http.Response res = await http.get(url);
-  return parseLatestUpdatedBandsList(JSON.decode(res.body));
+  return parseLatestUpdatedBandsList(jsonDecode(res.body));
 }
 
 getLatestUpdatedLabels(int year, int month) async {
   String url = latestUpdatedLabels(year, month);
   http.Response res = await http.get(url);
-  return parseLatestUpdatedLabelsList(JSON.decode(res.body));
+  return parseLatestUpdatedLabelsList(jsonDecode(res.body));
 }
 
 getLatestUpdatedArtists(int year, int month) async {
   String url = latestUpdatedArtists(year, month);
   http.Response res = await http.get(url);
-  return parseLatestUpdatedArtistsList(JSON.decode(res.body));
+  return parseLatestUpdatedArtistsList(jsonDecode(res.body));
 }
 
 getBand(String name, int id) async {
