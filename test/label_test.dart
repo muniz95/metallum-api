8149e3b626238505eab1706dbe0b99f6530a6749
Rodrigo Muniz <rodrigo.temiski1995@gmail.com>
2018-09-06 12:56:44 -0300

    Label test specification

---
+++ test/label_test.dart
@@ -0,0 +1,11 @@
+import 'package:test/test.dart';
+
+void main() {
+  group('Label', () {
+    group('search', () {
+      test('should bring at least one label, given a name', () {
+        expect(false, true);
+      });
+    });
+  });
+}
