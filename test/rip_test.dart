8f367af1087a1bb90c0bfb6b36e66bf19422deab
Rodrigo Muniz <rodrigo.temiski1995@gmail.com>
2018-09-06 13:26:09 -0300

    RIP test specification

---
+++ test/rip_test.dart
@@ -0,0 +1,14 @@
+import 'package:test/test.dart';
+
+void main() {
+  group('RIP', () {
+    group('search', () {
+      test('should bring a list with 200 records if no filter is applied', () {
+        expect(false, true);
+      });
+      test('should bring a list of records, given a year', () {
+        expect(false, true);
+      });
+    });
+  });
+}
